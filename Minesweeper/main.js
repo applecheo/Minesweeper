2c652c854073109a862a47a9c9c0f4bd7f4ceaa8
junjiecheo12 <wkcheojj@gmail.com>
2022-08-06 23:14:15 +0800

    add flag and check for mine

--- Minesweeper/main.js
+++ Minesweeper/main.js
@@ -3,17 +3,18 @@ console.log($);
 
 const app = {
   board: [
-    ["?", "?", "?", "?", "?", "?", "?", "?"],
-    ["?", "?", "?", "?", "?", "?", "?", "?"],
-    ["?", "?", "?", "?", "?", "?", "?", "?"],
-    ["?", "?", "?", "?", "?", "?", "?", "?"],
-    ["?", "?", "?", "?", "?", "?", "?", "?"],
-    ["?", "?", "?", "?", "?", "?", "?", "?"],
-    ["?", "?", "?", "?", "?", "?", "?", "?"],
-    ["?", "?", "?", "?", "?", "?", "?", "?"],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
   ],
   bomb: 8,
-  flag: 8,
+  flag: false,
   page: "#startPage",
 };
 
@@ -34,24 +35,58 @@ const renderBoard = () => {
   }
 };
 
-//adding bomb
+//add bomb
 const generateBomb = () => {
   let arr = [];
   while (arr.length < app.bomb) {
-    let randomNum = Math.floor(Math.random() * 8);
-    let randomNum2 = Math.floor(Math.random() * 8);
+    let randomNum = Math.floor(Math.random() * app.bomb);
+    let randomNum2 = Math.floor(Math.random() * app.bomb);
     if (arr.includes(randomNum + "-" + randomNum2) === false) {
       arr.push(randomNum + "-" + randomNum2);
     }
   }
-  for (let bomb of arr) {
+  for (const bomb of arr) {
     $("#" + bomb).addClass("hasBomb");
   }
   return arr;
 };
 
+//add flag
+document.body.onkeyup = (e) => {
+  if (e.keyCode == 32 && app.flag === false) {
+    $("#toggleFlag").removeClass("hide");
+    app.flag = true;
+  } else if (e.keyCode == 32 && app.flag === true) {
+    $("#toggleFlag").addClass("hide");
+    app.flag = false;
+  }
+};
+
+const addFlag = (e) => {
+  if (app.flag === true && $(e.target).hasClass("flag")) {
+    $(e.target).removeClass("flag");
+  } else if (app.flag === true) {
+    $(e.target).addClass("flag");
+  }
+};
+
+//check for mine
+const checkMine = (e) => {
+  if ($(e.target).hasClass("hasBomb")) {
+    alert("dead");
+  } else {
+    alert("not checked");
+  }
+};
+
+//mine count
+const mineCount = () => {
+  return $(".mineCount").text(app.bomb);
+};
+
 const clicked = () => {
-  $(".board").on("click", () => console.log("hello"));
+  $(".board").on("click", checkMine);
+  $(".board").on("click", addFlag);
 };
 
 const render = () => {
@@ -60,6 +95,7 @@ const render = () => {
   renderBoard();
   generateBomb();
   clicked();
+  mineCount();
 };
 render();
 
